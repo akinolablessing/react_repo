56845816b0f5daba656a469fd34b3e9fac984302
akinolablessing <akinolablessing1890@gmail.com>
2025-05-04 15:12:00 +0100

    done with adding

--- To-Do-List/src/App.jsx
+++ To-Do-List/src/App.jsx
@@ -1,4 +1,4 @@
-import React,{ useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import reactLogo from './assets/react.svg'
 import { MdDelete } from "react-icons/md";
 import './App.css'
@@ -6,7 +6,26 @@ import {BsCheckLg} from "react-icons/bs";
 
 function App() {
 const [isCompletedScreen,setIsCompletedScreen] = useState(false);
+const [allTodos,setTodos] = useState([]);
+const [newTitle,setNewTitle] = useState("");
+const [newDescription,setNewDescription] = useState("");
+const handleAddTodo =()=>{
+let newTodoItem ={
+    title:newTitle,
+    description:newDescription
+}
+let updatedTodoArr= [...allTodos];
+    updatedTodoArr.push(newTodoItem);
+    setTodos(updatedTodoArr);
+    localStorage.setItem("todolist",JSON.stringify(updatedTodoArr))
+};
 
+    useEffect(() => {
+        let savedTodo =JSON.parse(localStorage.getItem("todolist"));
+        if(savedTodo){
+            setTodos(savedTodo);
+        }
+    }, []);
   return (
     <>
       <div className="App">
@@ -16,14 +35,14 @@ const [isCompletedScreen,setIsCompletedScreen] = useState(false);
             <div className="todo-input">
                 <div className="todo-input-item">
                     <label>Title</label>
-                    <input type="text" placeholder="what's the task title?" />
+                    <input type="text" value={newTitle} onChange={(e)=>setNewTitle(e.target.value)} placeholder="what's the task title?" />
                 </div>
                 <div className="todo-input-item">
                     <label>Description</label>
-                    <input type="text" placeholder="what's the task description?" />
+                    <input type="text" value={newDescription} onChange={(e)=>setNewDescription(e.target.value)} placeholder="what's the task description?" />
                 </div>
                 <div className="todo-input-item">
-                    <button type="button" className="primaryBtn">Add</button>
+                    <button type="button" onClick={handleAddTodo} className="primaryBtn">Add</button>
                 </div>
             </div>
               <div className="buttonArea">
@@ -34,16 +53,24 @@ const [isCompletedScreen,setIsCompletedScreen] = useState(false);
               </div>
 
               <div className="todo-list">
-                <div className="todo-list-item">
+                  {allTodos.map((item,index)=>{
+                      return(
+                          <div className="todo-list-item" key={index}>
+                              <div>
+                                  <h3>{item.title}</h3>
+                                  <p>{item.description}</p>
+                              </div>
+                              <div>
+                                  <MdDelete className="icon"/>
+                                  <BsCheckLg className="check-icon"/>
 
-                    <h3>Task 1</h3>
-                    <p>Description</p>
-                </div>
-                  <div>
-                      <MdDelete className="icon"/>
-                      <BsCheckLg className="check-icon"/>
+                              </div>
+                          </div>
+                      )
+                  })}
 
-                  </div>
+
+
               </div>
           </div>
       </div>
