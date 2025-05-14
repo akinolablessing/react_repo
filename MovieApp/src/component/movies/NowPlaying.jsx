f0b6ab4bee071e0aff6957ff61fd776207ea2797
akinolablessing <akinolablessing1890@gmail.com>
2025-05-14 12:12:12 +0100

    humm

--- MovieApp/src/component/movies/NowPlaying.jsx
+++ MovieApp/src/component/movies/NowPlaying.jsx
@@ -1,8 +1,13 @@
 import React from "react";
-
+import {useGetNowPlaingMovies} from "../../services/movieApi.jsx";
+import MovieCard from "../../reuseable/MovieCard.jsx"
 const NowPlaying = () =>{
+    const data = useGetNowPlaingMovies()
+
     return(
-        <div>NowPlaying</div>
+        <div>
+            <MovieCard data={data}/>
+        </div>
     )
 }
 export default NowPlaying
--- MovieApp/src/store/store.jsx
+++ MovieApp/src/store/store.jsx
@@ -2,7 +2,7 @@ import {configureStore} from "@reduxjs/toolkit";
 // import {setupListeners} from "@reduxjs/toolkit/query";
 import {userAuthApiSlice} from "../services/userAuthApi.jsx";
 import {setupListeners} from "@reduxjs/toolkit/query";
-
+import {movieApi} from "../services/movieApi.jsx";
 
 export const store = configureStore({
     reducer:{
