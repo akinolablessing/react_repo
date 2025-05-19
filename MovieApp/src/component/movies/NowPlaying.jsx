7f6af98b3bc82b67c798470c98c1420593149765
akinolablessing <akinolablessing1890@gmail.com>
2025-05-19 10:06:25 +0100

    all

--- MovieApp/src/component/movies/NowPlaying.jsx
+++ MovieApp/src/component/movies/NowPlaying.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import {useGetNowPlayingMoviesQuery} from "../../services/movieApi.jsx";
 import MovieCard from "../../reuseable/MovieCard.jsx"
+// import  style from "./NowPlaying.module.css"
 const NowPlaying = () =>{
+
     const data = useGetNowPlayingMoviesQuery()
     console.log(data)
-
     return(
         <div>
             <MovieCard data={data}/>
